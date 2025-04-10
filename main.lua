dd61a2e3314801469d520a6deae1da26011c0f01
Alan Sartorio <alan42ga@hotmail.com>
2025-04-09 19:16:53 -0700

    make table responsive

--- main.lua
+++ main.lua
@@ -56,14 +56,55 @@ function love.load()
     roulette_table = RouletteTable.new()
 end
 
+---@class Box
+---@field pos Vector
+---@field size Vector
+
+---@param box_size Vector
+---@param space Box
+---@return Box
+local function center_box_in_space(box_size, space)
+    return {
+        pos = (space.pos + space.size / 2) - box_size / 2,
+        size = box_size
+    }
+end
+
+---@param available_size Vector
+---@param aspect_ratio Vector
+---@return Vector
+local function find_size_for_aspect_ratio(available_size, aspect_ratio)
+    local ar = aspect_ratio.y / aspect_ratio.x
+    local width = available_size.x
+    local estimated_height = width * ar
+    if estimated_height > available_size.y then
+        return Vector.new(available_size.y / ar, available_size.y)
+    else
+        return Vector.new(width, estimated_height)
+    end
+end
+
 function get_positioning()
     local roulette_start = 0
     local split = 0.3
     local table_start = win.x * split
 
+    local table_space_box = {
+        pos = Vector.new(table_start, 0),
+        size = Vector.new(win.x - table_start, win.y),
+    }
+    local table_cell_size = Vector.new(14, 5)
+    local table_size = find_size_for_aspect_ratio(table_space_box.size, table_cell_size)
+    local table_box = center_box_in_space(table_size, table_space_box)
+    local cell_size = (table_size / table_cell_size).x
+    roulette_table.cell_size = cell_size
+
     return {
         roulette = Vector.new((table_start - roulette_start) / 2, win.y / 2),
-        roulette_table = Vector.new(table_start, 0)
+        roulette_table = {
+            cell_size = cell_size,
+            pos = table_box.pos
+        }
     }
 end
 
@@ -74,7 +115,7 @@ function love.mousereleased(x, y, button)
     local positioning = get_positioning()
     local mouse = Vector.new(x, y)
     if button == 1 then
-        local cell = roulette_table:get_cell(mouse - positioning.roulette_table)
+        local cell = roulette_table:get_cell(mouse - positioning.roulette_table.pos)
         if roulette.rolling then
             return
         end
@@ -99,7 +140,7 @@ function love.update(dt)
     --for _ = 1, 100000 do
     --end
     roulette:update(dt)
-    local cell = roulette_table:get_cell(Vector.new(love.mouse.getPosition()) - positioning.roulette_table)
+    local cell = roulette_table:get_cell(Vector.new(love.mouse.getPosition()) - positioning.roulette_table.pos)
     if cell ~= nil then
         --print(unpack(cell))
         hover_pos = roulette_table:get_cell_center(cell)
@@ -126,7 +167,7 @@ function love.draw()
     love.graphics.pop()
 
     love.graphics.push()
-    love.graphics.translate(positioning.roulette_table:unpack())
+    love.graphics.translate(positioning.roulette_table.pos:unpack())
     roulette_table:draw()
     love.graphics.setColor(1, 1, 1, 0.3)
     if hover_pos ~= nil then
